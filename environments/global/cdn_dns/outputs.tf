139ed6eb71c97a3f695b2b89820e98a6d6f8d1b3
QaysAlnajjad <qaysalnajjad317@gmail.com>
2025-11-25 16:22:38 +0400

    Initial commit: AWS Multi-Region WordPress DR architecture

--- environments/global/cdn_dns/outputs.tf
+++ environments/global/cdn_dns/outputs.tf
@@ -3,17 +3,17 @@ output "primary_domain" {
 }
 
 output "media_distribution_id" {
-  value = module.cdn.media_distribution_id
+  value = module.cdn_dns.media_distribution_id
 }
 
 output "media_distribution_domain" {
-  value = module.cdn.media_distribution_domain
+  value = module.cdn_dns.media_distribution_domain
 }
 
 output "media_distribution_arn" {
-  value = module.cdn.media_distribution_arn
+  value = module.cdn_dns.media_distribution_arn
 }
 
 output "app_distribution_arn" {
-  value = module.cdn.app_distribution_arn
+  value = module.cdn_dns.app_distribution_arn
 }
