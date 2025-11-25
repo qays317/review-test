1bd6747d31d3aac459ea735dd2bb9ab36ebfcd1e
QaysAlnajjad <qaysalnajjad317@gmail.com>
2025-11-25 20:52:02 +0400

    Initial commit: AWS Multi-Region WordPress DR architecture

--- environments/global/cdn_dns/variables-cdn.tf
+++ environments/global/cdn_dns/variables-cdn.tf
@@ -19,6 +19,7 @@ variable "cloudfront_distribution_config" {
 
 variable "provided_ssl_certificate_arn" {                
     type = string
+    default = ""
 }
 
 variable "hosted_zone_id" {                    
