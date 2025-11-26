d507e55eb6f0ca8423e4e4fe5edb7fdac774798a
qays317 <qaysalnajjad317@gmail.com>
2025-11-26 18:03:59 +0400

    Update config.sh

--- scripts/deployment-automation-scripts/config.sh
+++ scripts/deployment-automation-scripts/config.sh
@@ -7,8 +7,8 @@ DR_REGION="ca-central-1"
 ############################################
 #  Terraform Backend Config
 ############################################
-TF_STATE_BUCKET_NAME="terraform-state-10112025"
-TF_STATE_BUCKET_REGION="eu-central-1"
+TF_STATE_BUCKET_NAME="github-state-bucket-26112025"
+TF_STATE_BUCKET_REGION="us-east-1"
 
 ############################################
 #  Docker / Container Config
@@ -19,20 +19,20 @@ ECR_REPO_NAME="ecs-wordpress-app"
 ############################################
 #  Media S3 buckets
 ############################################
-PRIMARY_MEDIA_S3_BUCKET="wordpress-media-200"
-DR_MEDIA_S3_BUCKET="wordpress-media-dr-200"
+PRIMARY_MEDIA_S3_BUCKET="wordpress-media-2001"
+DR_MEDIA_S3_BUCKET="wordpress-media-dr-2001"
 
 ############################################
 #  Domain and hosted zone
 ############################################
-PRIMARY_DOMAIN="qays.cloud" # Primary custom domain without www (e.g., yourdomain.com)
-HOSTED_ZONE_ID="Z03824873AC3XLDK55Q1"
+PRIMARY_DOMAIN="rqays.com" # Primary custom domain without www (e.g., yourdomain.com)
+HOSTED_ZONE_ID="Z046647128J97ELQJFGYW"
 
 
 ############################################
 #  SSL certificates
 ############################################
-PRIMARY_ALB_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-1:127214183643:certificate/3da9d899-59ce-43a4-8c85-abca7b5e159f"
+PRIMARY_ALB_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-1:156166604445:certificate/e061179d-aea4-41bb-a266-9ecabb227bbe"
 DR_ALB_SSL_CERTIFICATE_ARN=""
-CLOUDFRONT_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-1:127214183643:certificate/3da9d899-59ce-43a4-8c85-abca7b5e159f"
+CLOUDFRONT_SSL_CERTIFICATE_ARN="arn:aws:acm:us-east-1:156166604445:certificate/e061179d-aea4-41bb-a266-9ecabb227bbe"
 
