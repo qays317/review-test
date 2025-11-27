7a08b0dcbadce19f93e2abdcd6bb73138904e679
qays317 <qaysalnajjad317@gmail.com>
2025-11-27 13:18:48 +0400

    Update main.tf

--- modules/cdn_dns/main.tf
+++ modules/cdn_dns/main.tf
@@ -190,7 +190,7 @@ ordered_cache_behavior {
 
 # Route 53 DNS records for www and root domains
 resource "aws_route53_record" "main" {
-  for_each = toset(local.domains)
+  for_each = toset([var.primary_domain])
   zone_id = var.hosted_zone_id
   name = each.value
   type = "A"
