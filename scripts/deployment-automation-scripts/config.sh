f240491f99e83ba47ac6de1890a08e9ac69ae88c
qays317 <qaysalnajjad317@gmail.com>
2025-11-28 22:56:06 +0400

    Update config.sh

--- scripts/deployment-automation-scripts/config.sh
+++ scripts/deployment-automation-scripts/config.sh
@@ -13,7 +13,7 @@ TF_STATE_BUCKET_REGION="eu-central-1"
 ############################################
 #  Docker / Container Config
 ############################################
-DOCKERHUB_IMAGE="qaysalnajjad/ecs-wordpress-app:v2.26"
+DOCKERHUB_IMAGE="qaysalnajjad/ecs-wordpress-app:v2.27"
 ECR_REPO_NAME="ecs-wordpress-app"
 
 ############################################
