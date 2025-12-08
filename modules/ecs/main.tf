369bfbe3c9db07d4c8d1c1fc648a70f584b6bc03
QaysAlnajjad <qaysalnajjad317@gmail.com>
2025-12-08 22:29:43 +0400

    Second commit: AWS Multi-Region WordPress DR architecture

--- modules/ecs/main.tf
+++ modules/ecs/main.tf
@@ -40,11 +40,7 @@ locals {
         {
           name = "AWS_S3_BUCKET"       # Used by AS3CF, WP Offload Media needs bucket name
           value = var.s3_bucket_name
-        },/*
-        {
-          name = "CLOUDFRONT_DOMAIN"   # Used by AS3CF
-          value = var.cloudfront_distribution_domain
-        },        
+        },  
         {
           name = "WORDPRESS_URL"
           value = "https://${var.primary_domain}"
@@ -56,7 +52,7 @@ locals {
         {
           name  = "CLOUDFRONT_DOMAIN"
           value = var.primary_domain
-        }*/
+        }
       ]
 
       secrets = [
